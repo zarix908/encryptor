d5a6d4619d3e18d02c8e26cbe687055ebdc3f029
zarix908 <daniyarrus@gmail.com>
2025-09-14 13:08:55 +0300

    fix misprint

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -r build/*
 zig build
 linuxdeploy --appdir=./build/AppDir --desktop-file=./build_deps/encrypter.desktop --icon-file=./build_deps/icon.svg --executable=./zig-out/bin/encrypter
 
-# deploy binarydependencies
+# deploy binary dependencies
 linuxdeploy --appdir=./build/AppDir --executable=./build_bin/keepassxc-cli
 linuxdeploy --appdir=./build/AppDir --executable=./build_bin/age-keygen
 linuxdeploy --appdir=./build/AppDir --executable=./build_bin/age
